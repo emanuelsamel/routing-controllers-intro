3f4c5c2f11a223f45a567f5e5bb82a35ebf21a03
emanuelsamel <mannys94@hotmail.com>
2017-06-08 16:57:58 -0400

    image_url"

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,10 +1,19 @@
 class PagesController < ApplicationController
 
   def welcome
-
+@header = "This is the welcome page"
   end
 
   def about
+@header = "Ruby rails is the best"
+  end
 
+  def contest
+@header = "Your the contest winner!"
+  end
+
+  def kitten
+    requested_size = params[:size]
+    @kitten_url = "http://lorempixel.com/#{requested_size}/#{requested_size}/cats"
   end
 end
